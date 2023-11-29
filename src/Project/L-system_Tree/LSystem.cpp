ca38c70dfeaac426600ac8b229687057d4ed7a11
Redcheeks <lillyoleole@gmail.com>
2023-11-29 15:37:25 +0100

    implemented some parts of L-system

--- src/Project/L-system_Tree/LSystem.cpp
+++ src/Project/L-system_Tree/LSystem.cpp
@@ -10,7 +10,8 @@ LSystem::LSystem()
 
 // the axiom parameter is a map that stores the variabels and corrosponding rules.
 
-//!< Add variable and rule to the axioms parameter.
+//!< Add variable and its production rule to the axioms parameter.
+//! Production rules are defined by  key -> rule
 void LSystem::AddAxiom(char key, std::string rule)
 {
 	axioms[key] = rule;
@@ -24,21 +25,34 @@ void LSystem::ClearAxioms()
 
 //if no rule exists for a variable then the rule is " var -> var "
 
-//!< Applies axiom rules to the input, returns a string representing the new system.
+//!< Applies axiom rules to the input which defines the system state.
+//!  returns a string representing the new system.
 std::string LSystem::ApplyAxioms(std::string system)
 {
 	std::string newSystem = ""; //Create an empty string.
 
-	for (int j=0; j < system.length())
+	for (int j = 0; j < system.length(), j++) { //run rules for each item from the input.
+
+
+	}
 
 	return newSystem;
 }
 
-//!< Applies axiom rules to the input for a certain amont of iterations, returns a string representing the new system.
-std::string LSystem::ApplyAxioms(std::string system, int iterations)
+//!< Applies axiom rules to the input and then the result... for *iterations, returns a string representing the new system.
+std::string LSystem::ApplyAxioms(std::string system, int iter)
 {
-	std::string newSystem = ""; //Create an empty string.
+	//Check basecase
+	if (iter <= 0) { return system; }
 
+
+	std::string newSystem = system; //
+
+	//Apply rules *iter* many times
+	for (int k = 0; k < iter; k++) {
+
+		newSystem = ApplyAxioms(newSystem);
+	}
 
 	return newSystem;
 }
