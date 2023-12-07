dc4a1d3882bee20093c5d09f7a551a1d41543bcf
Jonas Andreasson <jo7334an-s@student.lu.se>
2023-12-07 14:59:08 +0100

    Working

--- src/Project/L-system_Tree/Branch.cpp
+++ src/Project/L-system_Tree/Branch.cpp
@@ -14,7 +14,7 @@ Branch::Branch(float radius, float height, glm::vec3 position, float angle, glm:
 	//get_transform().SetTranslate(position);
 	//calculate the positions.
 	start_pos = position;
-	end_pos = position+glm::vec3((glm::vec4(0,height,0,0)*glm::rotate(glm::mat4(1.0f), angle, rotation))); //multiply with
+	end_pos = position+glm::vec3(glm::rotate(glm::mat4(1.0f), angle, rotation)* glm::vec4(0, height, 0, 0)); //multiply with
 	std::cout << "Start pos:" << start_pos << '\n';
 	std::cout << "End pos:" << end_pos << '\n';
 }
