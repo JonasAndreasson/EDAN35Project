b06f65420887b7decd8b70f8ebc4557c7735ce05
Redcheeks <lillyoleole@gmail.com>
2023-12-09 15:11:03 +0100

    skybox mesh

--- src/Project/project_main.cpp
+++ src/Project/project_main.cpp
@@ -42,54 +42,45 @@ project::ProjectMain::~ProjectMain()
 	bonobo::deinit();
 }
 
+
 void
 project::ProjectMain::run()
 {
 
-	//Create L-system
-	/*
-	* Using ´Fifo Stack.
+	
+	 //Create L -system
+		/*
+		* Using ´Fifo Stack.
 			"-" : turn left 45°
 			"+" : turn right 45°.
 			"F" : draw branch
 			"X" : Dummy command - do nothing, used for organic generation
 			"[" : push position
-			"]" : pop position (here we can add leaves?
-	*/
+			"]" : pop position  TODO:here we can add leaves?
+		*/
+		LSystem fractalSys;
+		fractalSys.AddAxiom('F', "F[-F][+F]");
+		fractalSys.angle = 0.4485496;
 
-	LSystem fractalSys;
-	fractalSys.AddAxiom('F', "F[-F][+F]");
-	fractalSys.angle = 0.4485496;
+		LSystem shrubby;
+		shrubby.AddAxiom('F', "F[+F]F[-F][F]");
+		shrubby.angle = 0.4485496;
+		shrubby.radius = 0.005;
+		shrubby.height = 0.06;
+		shrubby.down_scaling = 0.9;
+		shrubby.down_scaling_height = 0.9;
+
+		LSystem treeSys1;
+		treeSys1.AddAxiom('F', "F-[-F+F+F]+[+F-F-F]");
+		treeSys1.angle = 0.3926991;
+
+		LSystem treeSys2;
+		treeSys2.AddAxiom('F', "F[-F+F+F][+F-F-F]");
+		treeSys2.angle = 0.3926991;
 	
 
-	LSystem shrubby;
-	shrubby.AddAxiom('F', "F[+F]F[-F][F]");
-	shrubby.angle = 0.4485496;
-	shrubby.radius = 0.005;
-	shrubby.height = 0.06;
-	shrubby.down_scaling = 0.9;
-	shrubby.down_scaling_height = 0.9;
 
-
-
-	
-
-	LSystem treeSys1;
-	treeSys1.AddAxiom('F', "F-[-F+F+F]+[+F-F-F]");
-	treeSys1.angle = 0.3926991;
-
-
-	LSystem treeSys2;
-	treeSys2.AddAxiom('F', "F[-F+F+F][+F-F-F]");
-	treeSys2.angle = 0.3926991;
-
-
-
-	// Load the sphere geometry
-	//auto const shape = parametric_shapes::createCircleRing(2.0f, 0.75f, 40u, 4u);
-	//auto const shape = parametric_shapes::createQuad(0.25f, 0.15f);
-	//auto const shape = parametric_shapes::createSphere(0.15f, 10u, 10u);
-	//auto const shape = parametric_shapes::createTorus(0.3f,0.15f, 100u, 100u);
+	// Load the geometry
 	auto const shape = parametric_shapes::createBranch(0.05f, 0.3f);
 	auto const branch2 = parametric_shapes::createBranch(0.025f, 0.15f);
 	if (shape.vao == 0u)
@@ -99,6 +90,7 @@ project::ProjectMain::run()
 	mCamera.mWorld.SetTranslate(glm::vec3(0.0f, 1.0f, 1.0f));
 	mCamera.mMouseSensitivity = glm::vec2(0.003f);
 	mCamera.mMovementSpeed = glm::vec3(3.0f); // 3 m/s => 10.8 km/h
+	auto camera_position = mCamera.mWorld.GetTranslation();
 
 	
 	// Create the shader programs
@@ -113,6 +105,25 @@ project::ProjectMain::run()
 		return;
 	}
 
+		//Sampler
+	auto sampl = [](GLuint a) {
+		glSamplerParameteri(a, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	};
+
+	GLuint sampler = bonobo::createSampler(sampl);
+		
+
+		// Skybox 
+	GLuint skybox_shader = 0u;
+	program_manager.CreateAndRegisterProgram("Skybox",
+		{ { ShaderType::vertex, "project/skybox.vert" },
+		  { ShaderType::fragment, "project/skybox.frag" } },
+		skybox_shader);
+	if ((skybox_shader) == 0u)
+		LogError("Failed to load skybox shader");
+	
+
+		//Diffuse, Normal, Tangent, Binormal, teexcoord - These we can maybe remove??
 	GLuint diffuse_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Diffuse",
 	                                         { { ShaderType::vertex, "project/diffuse.vert" },
@@ -157,47 +168,67 @@ project::ProjectMain::run()
 	auto const set_uniforms = [&light_position](GLuint program){
 		glUniform3fv(glGetUniformLocation(program, "light_position"), 1, glm::value_ptr(light_position));
 	};
-
 	// Set the default tensions value; it can always be changed at runtime
 	// through the "Scene Controls" window.
 	float catmull_rom_tension = 0.5f;
-
 	// Set whether the default interpolation algorithm should be the linear one;
 	// it can always be changed at runtime through the "Scene Controls" window.
 	bool use_linear = true;
-
 	// Set whether to interpolate the position of an object or not; it can
 	// always be changed at runtime through the "Scene Controls" window.
 	bool interpolate = true;
-
 	// Set whether to show the control points or not; it can always be changed
 	// at runtime through the "Scene Controls" window.
 	bool show_control_points = true;
+
+		// Tree Bark shader
 	GLuint bark = 0u;
-	program_manager.CreateAndRegisterProgram("Texture coords",
+	program_manager.CreateAndRegisterProgram("Tree_Bark",
 		{ { ShaderType::vertex, "project/texture.vert" },
 		  { ShaderType::fragment, "project/texture.frag" } },
 		bark);
 	if (bark == 0u)
-		LogError("Failed to load texcoord shader");
+		LogError("Failed to load treebark shader");
 
-	
+
 	auto const bark_uniforms = [&light_position](GLuint program) {
 		glUniform3fv(glGetUniformLocation(program, "light_position"), 1, glm::value_ptr(light_position));
 		};
-	auto circle_rings = Node();
+	//auto circle_rings = Node();
 	//auto second_branch = Branch(0.025f, 0.15f, glm::vec3(0,0,0), 0, glm::vec3(1.0),0.5f);
 
 
+	GLuint cubemap = bonobo::loadTextureCubeMap(
+		config::resources_path("cubemaps/SemiSky/sky-right.png"),
+		config::resources_path("cubemaps/SemiSky/sky-left.png"),
+		config::resources_path("cubemaps/SemiSky/sky-top.png"),
+		config::resources_path("cubemaps/SemiSky/sky-bottom.png"),
+		config::resources_path("cubemaps/SemiSky/sky-front.png"),
+		config::resources_path("cubemaps/SemiSky/sky-back.png")
+	);
 
-	std::string s = shrubby.ApplyAxioms("F", 5);
-	std::cout << s << '\n';
+
+	// --- Shaders done 
+
+	auto skybox_shape = parametric_shapes::createSphere(100.0f, 100u, 100u);
+	if (skybox_shape.vao == 0u) {
+		LogError("Failed to retrieve the mesh for the skybox");
+		return;
+	}
+	Node skybox, surface;
+	skybox.set_geometry(skybox_shape);
+	skybox.add_texture("cubemap", cubemap, GL_TEXTURE_CUBE_MAP);
+	skybox.set_program(&skybox_shader);
+
+
+	std::string s = shrubby.ApplyAxioms("F", 5); //TODO: This could be done in tree...?
+	//std::cout << s << '\n';
 	GLuint diff_texture = bonobo::loadTexture2D(config::resources_path("textures/BarkPoplar001_COL_4K.jpg"));
 	Tree tree = Tree(s, shrubby, glm::vec3(0, 0, 0),&bark, bark_uniforms, diff_texture);
 	
-	circle_rings.set_geometry(shape);
-	circle_rings.set_program(&normal_shader, set_uniforms);
-	TRSTransformf& circle_rings_transform_ref = circle_rings.get_transform();
+	//circle_rings.set_geometry(shape);
+	//circle_rings.set_program(&normal_shader, set_uniforms);
+	//TRSTransformf& circle_rings_transform_ref = circle_rings.get_transform();
 	//second_branch.set_program(&normal_shader, set_uniforms);
 	
 	
@@ -272,6 +303,9 @@ project::ProjectMain::run()
 			tree.get_child(i)->render(mCamera.GetWorldToClipMatrix());
 		}
 
+		skybox.get_transform().SetTranslate(glm::vec3(camera_position));
+		skybox.render(mCamera.GetWorldToClipMatrix());
+
 		
 
 		bool const opened = ImGui::Begin("Scene Controls", nullptr, ImGuiWindowFlags_None);
@@ -284,7 +318,7 @@ project::ProjectMain::run()
 
 			auto selection_result = program_manager.SelectProgram("Shader", program_index);
 			if (selection_result.was_selection_changed) {
-				circle_rings.set_program(selection_result.program, set_uniforms);
+				//circle_rings.set_program(selection_result.program, set_uniforms);
 				//second_branch.set_program(selection_result.program, set_uniforms);
 			}
 			ImGui::Separator();
